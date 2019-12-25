7381edd6521d8a9460b2b97c7062a5a04904a1d8
Khaled Sabry Fahmy <khaled_sabry_f@hotmail.com>
2019-12-25 14:39:20 -0700

    question_5

---
+++ UnitTestProject1/testModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VogCodeChallenge.API.modules;
+using Xunit;
+
+namespace UnitTestProject1
+{
+    public class testModel
+    {
+        [Theory]
+        [InlineData(1, 2)]//test doubler
+        [InlineData(2, 4)]//test doubler
+
+        [InlineData(3, 9)]//test tripler
+        [InlineData(4, 12)]//test tripler
+        [InlineData(5, 15)]//test tripler
+
+        [InlineData(1.0f, 0d)]//test leveler 
+
+        [InlineData(1.0d, 1.0d)] //test default
+        [InlineData(-2, -2)]//test default
+        [InlineData(2.0f, 2.0f)]//test default
+        [InlineData("aaa", "aaa")]//test default
+        public void test_TestModel(object input, object expected)
+        {
+           var result= TESTModule.Module(input);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(result, expected);
+        }
+    }
+}
---
+++ VogCodeChallenge.API/modules/TESTModule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VogCodeChallenge.API.modules
+{
+    public static class TESTModule
+    {
+        public static object Module(object testObject)
+        {
+            switch (testObject)
+            {
+                case 1:
+                case 2:
+                    return doubler((int)testObject);
+                case int value when value >= 3:
+                    return tripler(value);
+                case float floatValue when floatValue == 1.0f:
+                    return leveler(floatValue);
+                default:
+                    return testObject;
+            }
+
+
+            int doubler(int amount) => amount * 2;
+            int tripler(int amount) => amount * 3;
+            double leveler(double amount) => amount - 0.1f * 10f; // this function will be called only when amount=1.0d , in the older .Net frameworks the result would be 1d - 0.1f * 10f = -1.490116E-08= -0.0000001490116 but that been fixed in the newer .Net frame work , similar problem reported here https://stackoverflow.com/questions/4272803/why-is-0-110-1-not-equal-is-0?noredirect=1&lq=1
+        }
+    }
+
+}
